0c012b7308b4697dff78f207719ae0a43350899d
Jaideep <jaideepshetti55@gmail.com>
2026-06-01 21:26:09 +0530

    Add Fibonacci Iterative in Java

---
+++ java/FibonacciIterative.java
@@ -0,0 +1,18 @@
+public class FibonacciIterative {
+    static void printFibonacci(int n) {
+        int f1 = 0, f2 = 1, i;
+        if (n < 1) return;
+        System.out.print(f1 + " ");
+        for (i = 1; i < n; i++) {
+            System.out.print(f2 + " ");
+            int next = f1 + f2;
+            f1 = f2;
+            f2 = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        printFibonacci(n);
+    }
+}
