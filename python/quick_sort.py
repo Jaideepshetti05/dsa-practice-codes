1226380080903704a4f121e72f9e2501687bdb02
Jaideep <jaideepshetti55@gmail.com>
2026-06-01 21:25:33 +0530

    Add Quick Sort in Python

---
+++ python/quick_sort.py
@@ -0,0 +1,23 @@
+def partition(arr, low, high):
+    i = (low - 1)
+    pivot = arr[high]
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return (i + 1)
+
+def quick_sort(arr, low, high):
+    if len(arr) == 1:
+        return arr
+    if low < high:
+        pi = partition(arr, low, high)
+        quick_sort(arr, low, pi - 1)
+        quick_sort(arr, pi + 1, high)
+    return arr
+
+if __name__ == "__main__":
+    arr = [10, 7, 8, 9, 1, 5]
+    n = len(arr)
+    print("Sorted array:", quick_sort(arr, 0, n - 1))
